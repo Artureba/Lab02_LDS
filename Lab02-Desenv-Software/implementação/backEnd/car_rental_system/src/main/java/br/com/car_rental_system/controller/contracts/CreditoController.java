fcd1f415d7705e5409c8b8a5f72d463aab7ffc6b
JoaoYM <aguiar.prado12@gmail.com>
2024-10-02 19:36:23 -0300

    Remoção de comentários

--- Lab02-Desenv-Software/implementação/backEnd/car_rental_system/src/main/java/br/com/car_rental_system/controller/contracts/CreditoController.java
+++ Lab02-Desenv-Software/implementação/backEnd/car_rental_system/src/main/java/br/com/car_rental_system/controller/contracts/CreditoController.java
@@ -1,37 +1,3 @@
-// package br.com.car_rental_system.controller.contracts;
-
-// import java.util.List;
-
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-
-// import br.com.car_rental_system.entity.CreditContract;
-// import br.com.car_rental_system.service.contracts.CreditoService;
-
-// @RestController
-// @RequestMapping("/api/contratos/credito")
-// public class CreditoController {
-
-//     @Autowired
-//     private CreditoService creditoService;
-
-//     @GetMapping
-//     public List<CreditContract> listarContratosDeCredito() {
-//         return creditoService.listarTodos();
-//     }
-
-//     @PostMapping
-//     public ResponseEntity<CreditContract> criarCredito(@RequestBody Long contratoId) {
-//         CreditContract credito = creditoService.criarContratoDeCredito(contratoId);
-//         return ResponseEntity.ok(credito);
-//     }
-// }
-
 package br.com.car_rental_system.controller.contracts;
 
 import java.util.List;
